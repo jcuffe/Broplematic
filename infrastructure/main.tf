c29236b0bd365b1d34864af3dc99dda39fa7614b
Jared Cuffe <jcuffe@gmail.com>
2021-03-03 19:30:47 -0800

    Uses GCS backend for state. (#7)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,13 @@ provider "google" {
   zone = var.zone
 }
 
+terraform {
+  backend "gcs" {
+    bucket = "broplematic-infrastructure-storage"
+    prefix = "terraform/state"
+  }
+}
+
 resource "google_storage_bucket" "storage_bucket" {
-  name = "test-bucket-123151gdgdagbadg"
+  name = "test-bucket-123151gd-gdagbadg"
 }
