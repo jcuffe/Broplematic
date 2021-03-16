6aceda4ac789de8e92500fdf8c66442c4979cb89
Jared Cuffe <jcuffe@gmail.com>
2021-03-15 23:29:51 -0700

    Provisions kubernetes infrastructure. (#12)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project = var.project
-  region = var.region
-  zone = var.zone
+  region  = var.region
+  zone    = var.zone
 }
 
 terraform {
@@ -11,6 +11,93 @@ terraform {
   }
 }
 
-resource "google_storage_bucket" "storage_bucket" {
-  name = "tet-buket-13151gd-gda"
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id                 = var.project
+  name                       = "broplematic-cluster"
+  region                     = var.region
+  zones                      = [var.zone]
+  network                    = "vpc-01"
+  subnetwork                 = "name"
+  ip_range_pods              = "pods-range"
+  ip_range_services          = "services-range"
+  http_load_balancing        = false
+  horizontal_pod_autoscaling = true
+  network_policy             = false
+  enable_private_endpoint    = false
+  enable_private_nodes       = true
+  # master_ipv4_cidr_block     = "10.0.0.0/28"
+
+  node_pools = [
+    {
+      name               = "default-node-pool"
+      machine_type       = "e2-medium"
+      node_locations     = var.zone
+      min_count          = 1
+      max_count          = 2
+      local_ssd_count    = 0
+      disk_size_gb       = 100
+      disk_type          = "pd-standard"
+      image_type         = "COS"
+      auto_repair        = true
+      auto_upgrade       = true
+      service_account    = "projects/broplematic-kubernetes-prod/serviceAccounts/infrastructure-service-account@broplematic-kubernetes-prod.iam.gserviceaccount.com"
+      preemptible        = false
+      initial_node_count = 80
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = []
+
+    default-node-pool = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      # node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
+  }
 }
